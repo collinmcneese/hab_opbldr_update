ee5fb2e9ab3ef68790be34d2326c110c1dee529f
Collin McNeese <cmcneese@chef.io>
2020-05-15 08:08:45 -0500

    wip, gha
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- test/scripts/bootstrap.sh
+++ test/scripts/bootstrap.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Package must be built locally from host system before executing test-kitchen
-if [ ! -f /tmp/habitat/results/last_build.env ] ; then
-  echo 'ERROR:: Could not find last_build.env'
-  echo 'Please verify that /tmp/habitat is set to mount in kitchen.yml and that there is a local build to test from'
-  exit 1
-fi
-
-# Load variables from last_build.env
-source /tmp/habitat/results/last_build.env || . /tmp/habitat/results/last_build.env
-
 export HAB_LICENSE="accept-no-persist"
 export HAB_ORIGIN="${pkg_origin}"
 
@@ -26,13 +16,12 @@ else
 fi
 hab license accept
 hab pkg install core/hab-studio
-hab origin key generate
-hab pkg install collinmcneese/hab_sup_service
+hab origin key generate collinmcneese
+hab studio build .
+results/last_build.env
 
 echo "Installing ${pkg_artifact}"
-hab pkg install /tmp/habitat/results/${pkg_artifact}
-# bootstrapfile='/tmp/habitat/results/bootstrap.json'
-# echo "{\"bootstrap_mode\": \"true\"}" > ${bootstrapfile}
+hab pkg install results/${pkg_artifact}
 
 echo "Determine pkg_prefix for ${pkg_artifact}"
 pkg_prefix=$(find "/hab/pkgs/${pkg_origin}/${pkg_name}" -maxdepth 2 -mindepth 2 | sort | tail -n 1)
