ee3f68b971f8aa60f733aa424cbaabaf68864ca1
Collin McNeese <cmcneese@chef.io>
2020-05-15 08:33:18 -0500

    delivery updates
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- cookbooks/hab_opbldr_update/test/integration/default/default_test.rb
+++ cookbooks/hab_opbldr_update/test/integration/default/default_test.rb
@@ -3,29 +3,4 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://www.inspec.io/docs/reference/resources/
 
-control 'cookbook validations' do
-  impact 0.1
-  title 'Validate cookbook base components implemented successfully'
-  describe directory('/var/chef/habitat') do
-    it { should exist }
-  end
-  describe file('/var/chef/habitat/hab-bldr-update-pkgs-default.sh') do
-    it { should exist }
-  end
-  describe file('/var/chef/habitat/seed.toml') do
-    it { should exist }
-  end
-end
-
-control 'systemd unit checks' do
-  impact 0.1
-  title 'Check for creation of systemd units.'
-  # http://inspec.io/docs/reference/resources/systemd_service/
-  describe systemd_service('hab-opbldr-update-default.service') do
-    it { should be_installed }
-  end
-  describe systemd_service('hab-opbldr-update-default.timer') do
-    it { should be_installed }
-    it { should be_enabled }
-  end
-end
+# Reference cookbook, no tests to execute
